f69730038956a203027ccccaae9930dc815358f3
Michael E. Cotterell <mepcotterell@gmail.com>
2019-01-09 13:44:38 -0500

    submodule updates

--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]
-then
-    echo "Usage: $0 ceXX"
-    exit 1
-fi
+#if [[ $# -ne 1 ]]
+#then
+#    echo "Usage: $0 ceXX"
+#    exit 1
+#fi
 
-SUBMODULE="cs1302-$1"
+#SUBMODULE="cs1302-$1"
 
 git pull origin master
-cd "$SUBMODULE"
 git submodule init
 git submodule update
-git pull origin master
+
